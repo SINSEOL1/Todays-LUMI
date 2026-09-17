12266e0ff0b92d8accaed65d2a386e7daa8e9525
SINSEOL1 <109088262+SINSEOL1@users.noreply.github.com>
2026-09-18 07:30:32 +0900

    feat: add initial application shell

---
+++ src/TodaysLUMI/App.xaml.cs
@@ -0,0 +1,17 @@
+using System.Windows;
+
+namespace TodaysLUMI;
+
+public partial class App : Application
+{
+    private MainWindow? _mainWindow;
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        _mainWindow = new MainWindow();
+        MainWindow = _mainWindow;
+        _mainWindow.Show();
+    }
+}
