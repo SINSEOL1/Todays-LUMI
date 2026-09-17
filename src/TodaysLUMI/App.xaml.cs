78b2032293704544daf21f6a8b8cdb5f711116d5
SINSEOL1 <109088262+SINSEOL1@users.noreply.github.com>
2026-09-18 07:36:42 +0900

    fix: resolve WPF application type ambiguity

--- src/TodaysLUMI/App.xaml.cs
+++ src/TodaysLUMI/App.xaml.cs
@@ -2,7 +2,7 @@ using System.Windows;
 
 namespace TodaysLUMI;
 
-public partial class App : Application
+public partial class App : System.Windows.Application
 {
     private MainWindow? _mainWindow;
 
